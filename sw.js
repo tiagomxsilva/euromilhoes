e1f3640a76312d8a56b9dee00daadcbdbe3f9ed1
tiagomxsilva <tiagomrdsilva@gmail.com>
2026-01-02 22:51:34 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "euromilhoes-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./euromilhoes_v03.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
